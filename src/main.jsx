489ea9371bbd578e0b267095d855ca3b7b9d3482
david <230401@summit-edu.com.tw>
2023-08-25 16:01:38 +0800

    改回來上課畫面

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-import App from './Test.jsx'
+import App from './App.jsx'
+// import App from './Test.jsx'
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
 
